72ff8020b769816b26ad8fbf8082ef2383b05e3f
Sergio <sergio.sotomayor@pucp.edu.pe>
2022-06-22 18:31:57 -0500

    avance lista areas2

--- detalleLocacion/detalleLocacion.js
+++ detalleLocacion/detalleLocacion.js
@@ -5,20 +5,28 @@ $(document).ready(function () {
     const idLocacion = urlParams.get('locacion');
     $.ajax({
         method: "GET",
-        url: "https://pokeapi.co/api/v2/location/" + idLocacion,
+        url: "https://pokeapi.co/api/v2/location/",
         datatype: "json",
         crossDomain: true,
     }).done(function (data) {
+        let locacionName = data.name;
+        $("#labelLocacion").text("Locación " + locacionName);
+        let regionName = data.name;
+        $("#labelRegion").text("Región " + regionName);
         let listaAreas = data.results;
         let contenthtml = "";
-        for (let i = 0; i < listaAreas.length; i++){
+        for (let i = 0; i < listaAreas.length; i++) {
             contenthtml += "<tr>";
             contenthtml += "   <td>" + (i + 1) + "</td>";
             contenthtml += "   <td>" + listaAreas[i].name + "</td>";
-            contenthtml += "   <td><a class=\"btn btn-primary botonDetalle\" href='pokemon_encounters' role=\"button\">Ver Pokemons</a></td>";
-            contenthtml += "<tr>";
+            contenthtml += "   <td><a class=\"btn btn-primary botonDetalle\" href='detalleRegion/detalleRegion.html?region="+(i+1)+"' role=\"button\">Ver Pokemones</a></td>";
+            contenthtml += "</tr>";
         }
         $("#tablaAreas").html(contenthtml);
+
+        $("button").click(function (){
+
+        })
     }).fail(function (err) {
         alert("Ocurrio un error al cargar la página")
     });
