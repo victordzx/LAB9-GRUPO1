801e5a1c7c331c7e1d76df57c563093da1f9b77e
Víctor Díaz <diaz.victorm@pucp.edu.pe>
2022-06-21 19:13:39 -0500

    Initial commit

---
+++ detalleLocacion/detalleLocacion.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function () {
+    // Metodo de obtención de parámetros
+    const urlParams = new URLSearchParams(window.location.search);
+    const idLocacion = urlParams.get('locacion');
+    
+});
---
+++ detalleRegion/detalleRegion.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function () {
+    // Metodo de obtención de parámetros
+    const urlParams = new URLSearchParams(window.location.search);
+    const idRegion = urlParams.get('region');
+
+});
