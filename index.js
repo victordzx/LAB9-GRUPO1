a5a62f86a6204a07e80d07a2a06fc66fa5f0ff6a
Samantha Sanchez <94878885+SamanthaXimena@users.noreply.github.com>
2022-06-22 02:59:21 -0500

    avance lista Region

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-
 $(document).ready(function () {
+    $.ajax({
+        method: "GET",
+        url: "https://pokeapi.co/api/v2/region/" ,
+        datatype: "json",
+        crossDomain: true,
+    }).done(function (data) {
 
+
+        let listaRegiones = data.results;
+
+        let contenthtml = "";
+        $.each(listaRegiones, function (i, region) {
+            contenthtml += "<tr>";
+            contenthtml += "<td>" + (i + 1) + "</td>";
+            contenthtml += "<td>" + region.name + "</td>";
+            //contenthtml += "<td><a href='../detalleRegion/detalleRegion.html?region=" + slash[6] + "' class='btn btn-primary'>Detalles</a></td>";
+            contenthtml += "</tr>";
+        });
+        $("#body-paises").html(contenthtml);
+
+    }).fail(function (err) {
+        alert("Ocurrio un error al cargar la página")
+    });
 });
