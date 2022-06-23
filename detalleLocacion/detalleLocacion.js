e19eab84dc37d8b578debe87b3b590f4fd1bbf27
Sergio <sergio.sotomayor@pucp.edu.pe>
2022-06-22 22:38:18 -0500

    final commit

--- detalleLocacion/detalleLocacion.js
+++ detalleLocacion/detalleLocacion.js
@@ -20,15 +20,41 @@ $(document).ready(function () {
             contenthtml += "<tr>";
             contenthtml += "   <td>" + (i + 1) + "</td>";
             contenthtml += "   <td>" + listaAreas[i].name + "</td>";
-            contenthtml += "   <td><a class=\"btn btn-primary botonDetalle\" href=\"../detalleArea/detalleArea.html?area="+(i+1)+" role=\"button\">Ver Pokemones</a></td>";
+            let id = listaAreas[i].url.split("/");
+            contenthtml += "<td><a onclick=\"getpokemons(" + id[6] + ")\" class=\"btn btn-success text-white\">Ver pokemones</a></td>";
             contenthtml += "</tr>";
         }
-        $("#tablaAreas").html(contenthtml);
-        let btnreturn ="";
-        btnreturn = btnreturn +"<a class=\"btn btn-primary\" role=\"button\" href=\"../detalleRegion/detalleRegion.html?region="+idRegion[6]+"\">Regresar a la región</a>"
+        let btnreturn = "";
+        btnreturn = btnreturn + "<a class=\"btn btn-primary botonDetalle\" role=\"button\" href=\"../detalleRegion/detalleRegion.html?region=" + idRegion[6] + "\">Regresar a la región</a>"
         $("#retroceso").html(btnreturn);
         $("#tablaAreas").html(contenthtml);
     }).fail(function (err) {
         alert("Ocurrio un error al cargar la página")
-    });
+    })
 });
+
+function getpokemons(poke) {
+    let cardpokemons = "";
+    $.ajax({
+        type: "GET",
+        url: "https://pokeapi.co/api/v2/location-area/" + poke,
+        success: function (response) {
+            $("#areaSeleccionada").text("Pokemones en el área: " + response.name);
+            $.each(response.pokemon_encounters, function (key, value) {
+                let idpoke = value.pokemon.url.split("/");
+                cardpokemons = cardpokemons + "<div class=\"text-center col-2\" style='border: solid; margin-left: 50px; margin-right: 50px; margin-bottom: 50px'>" +
+                    "<img id=\"imagen" + idpoke[6] + "\" > <br>" + "<h6>" + value.pokemon.name + "</h6>" + "</div>"
+                $.ajax({
+                    type: "GET",
+                    url: "https://pokeapi.co/api/v2/pokemon/" + idpoke[6],
+                    success: function (response1) {
+                        $.each(response1.sprites, function (key1, value1) {
+                            $("#imagen" + idpoke[6]).attr('src', response1.sprites.back_default);
+                        });
+                    },
+                });
+            });
+            $("#pokemons").html(cardpokemons);
+        },
+    });
+}
